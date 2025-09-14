96ecff9feb9f85c3afa8fdd5b8d7f2ff7f7b505c
Yashodhan876 <yashodhanbankar11@gmail.com>
2025-09-14 19:57:06 +0530

    add all files

---
+++ src/test/java/com/example/SpringBootLearning/SpringBootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
